e8ce5d26a06cc8c0190d70e12d830cc9af85254f
Your Name <you@example.com>
2021-03-30 18:56:35 -0400

    Added variables to frame incremental function

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -1,18 +1,32 @@
+set -o errexit
+set -o nounset
+set -o pipefail
 
+#Directories to include:
+INCLUDES=(
+    "--include=/"
+)
 #Directories to exclude:
 EXCLUDES=(
-    "--exclude=/dev/*"
-    "--exclude=/var/tmp/*"
-    "--exclude=/var/lock/*"
-    "--exclude=/var/log/*"
-    "--exclude=/var/run/*"
-    "--exclude=/proc/*"
-    "--exclude=/sys/*"
-    "--exclude=/tmp/*"
-    "--exclude=/run/*"
-    "--exclude=/mnt/*"
-    "--exclude=/media/*"
-    "--exclude=/lost+found/*"
+    "--exclude=/dev/"
+    "--exclude=/var/tmp/"
+    "--exclude=/var/lock/"
+    "--exclude=/var/log/"
+    "--exclude=/var/run/"
+    "--exclude=/proc/"
+    "--exclude=/sys/"
+    "--exclude=/tmp/"
+    "--exclude=/run/"
+    "--exclude=/mnt/"
+    "--exclude=/media/"
+    "--exclude=/lost+found/"
 )
 
-sudo rsync -qahAXv "${EXCLUDES[@]}" --progress / /mnt/SLON/pingwin_backup/
+readonly DESTINATION="/mnt/SLON/pingwin_backup"
+readonly DATETIME="$(date '+%Y-%m-%d_%H:%M:S')"
+readonly BACKUP_PATH="${DESTINATION}/${DATETIME}"
+readonly LATEST_LINK="${DESTINATION}/latest"
+
+mkdir -p "${DESTINATION}"
+
+sudo rsync -ahAX "${EXCLUDES[@]}" --info=progress2 "${INCLUDES[@]}" /mnt/SLON/pingwin_backup/
