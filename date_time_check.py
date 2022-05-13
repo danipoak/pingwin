031613a277aaa7df38cc9db3eca3453620c038bc
Your Name <you@example.com>
2022-05-12 21:34:40 -0400

    Added record and reae functions

--- date_time_check.py
+++ date_time_check.py
@@ -1,5 +1,12 @@
 from datetime import date
 
-password_date - date.today()
+password_date = date.today()
 
-print('You have rest your password on:', pasword_date)
+print('You have rest your password on:', password_date)
+
+with open ('password_reset_date.txt', 'w') as f:
+    f.write(str(password_date))
+
+with open ('password_reset_date.txt', 'r') as g:
+    last_reset_date = g.readline()
+    print('The last time you reset your password was:', last_reset_date)
