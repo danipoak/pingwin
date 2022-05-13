57bb795658999f08201b882f98c5ae09638e1ec7
Your Name <you@example.com>
2022-05-12 22:01:12 -0400

    CLI parse works Added conditionals for CLI input

--- date_time_check.py
+++ date_time_check.py
@@ -11,18 +11,20 @@ import argparse
 from datetime import date
 
 parser = argparse.ArgumentParser(description='Process CLI flags')
-parser.add_argument('--write-date', action='store', help='writes the date your password is changed')
-parser.add_argument('--read-date', action='store', help='reads the last date your password was changed')
+parser.add_argument('--write', "-w", action='store_true', help='writes the date your password is changed')
+parser.add_argument('--read', '-r', action='store_true', help='reads the last date your password was changed')
+args = parser.parse_args()
 
-if 
-password_date = date.today()
+if args.write:
+    password_date = date.today()
 
-print('You have rest your password on:', password_date)
+    print('You have rest your password on:', password_date)
 
-with open ('password_reset_date.txt', 'w') as f:
-    f.write(str(password_date))
+    with open ('password_reset_date.txt', 'w') as f:
+        f.write(str(password_date))
 
-with open ('password_reset_date.txt', 'r') as g:
-    last_reset_date = g.readline()
-    print('The last time you reset your password was:', last_reset_date)
+if args.read:
+    with open ('password_reset_date.txt', 'r') as g:
+        last_reset_date = g.readline()
+        print('The last time you reset your password was:', last_reset_date)
 
