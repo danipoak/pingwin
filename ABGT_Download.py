676f5a1fabf9a7b1cf2e12d95a9202689c232956
Your Name <you@example.com>
2022-08-30 22:07:49 -0400

    updated functions

--- ABGT_Download.py
+++ ABGT_Download.py
@@ -13,18 +13,24 @@ br.set_handle_refresh(False)
 br.addheaders = [('User-agent', 'Firefox')]
 
 br.open('https://globaldjmix.com/livedj/above---beyond')
-episode_number = 449
+episode_number = 442
 episode_string = "abgt-" + str(episode_number)
 
 for l in br.links():
     if episode_string in l.url and 'autoplay' not in l.url:
-        br.follow_link(l) #hopefully all are fine and we pick one
+        br.follow_link(l) #follow link corresponding to episode
         break
 
 for l in br.links():
     if "box.download" in l.text:
         br.follow_link(l)
         break
+    elif "fex.download" in l.text:
+        br.follow_link(l)
+        break
+        
+for l in br.links():
+    print (l.url)
 
 #print(br.links()[0].url) #only one link on the final download page
                                 
